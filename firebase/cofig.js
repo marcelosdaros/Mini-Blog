89ef67fa040b2e6caab9d77baf5debcf48ff54d6
Daros <marcelo.sdaros@gmail.com>
2024-01-26 15:11:19 -0300

    fist commit

---
+++ firebase/cofig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqBL3HxgP-qYkCHj6qwNejSTstnWBGqsQ",
+  authDomain: "mini-blog-e4337.firebaseapp.com",
+  projectId: "mini-blog-e4337",
+  storageBucket: "mini-blog-e4337.appspot.com",
+  messagingSenderId: "910247508596",
+  appId: "1:910247508596:web:0a7f3e1a607fbbb9e6af21"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
